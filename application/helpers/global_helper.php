7123faa7f917b8a86d94944471ab5240be764f4d
irfan <irfan.isma@gmail.com>
2019-10-18 03:44:03 +0700

    merubah format rupiah di global helper

--- application/helpers/global_helper.php
+++ application/helpers/global_helper.php
@@ -67,9 +67,10 @@ function getSession($field = ''){
     return $result;
 }
 
-function uang( $var, $tipe=null, $dec="0" ){
+function uang( $var, $tipe=null, $dec = "0" ){
     if ( empty($var) ) return 0;
-    return 'Rp. ' . number_format(str_replace(',','.',$var), $dec,',','.').($dec=="0"?($tipe == true ? ',-' : ",00" ):'');
+    return 'Rp. ' . number_format(str_replace(',','.',$var), $dec,',','.');
+    // . ( $dec == "0" ? ( $tipe == true ? ',-' : ",00" ) : '' );
 }
 
 ?>
