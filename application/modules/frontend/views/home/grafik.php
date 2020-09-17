aa7d73ffdeaa140ebabcb2e78a5305eabb46fc16
Andre <andreindra100@gmail.com>
2020-09-17 15:46:19 +0700

    update

--- application/modules/frontend/views/home/grafik.php
+++ application/modules/frontend/views/home/grafik.php
@@ -90,18 +90,18 @@
 
         var previousPoint = null;
         $("#chart_2").bind("plothover", function(event, pos, item) {
-            $("#x").text(pos.x.toFixed(2));
-            $("#y").text(pos.y.toFixed(2));
+            $("#x").text(pos.x.toFixed(0));
+            $("#y").text(pos.y.toFixed(0));
 
             if (item) {
                 if (previousPoint != item.dataIndex) {
                     previousPoint = item.dataIndex;
 
                     $("#tooltip").remove();
-                    var x = item.datapoint[0].toFixed(2),
-                        y = item.datapoint[1].toFixed(2);
+                    var x = item.datapoint[0].toFixed(0),
+                        y = item.datapoint[1].toFixed(0);
 
-                    showTooltip(item.pageX, item.pageY, item.series.label + " of " + x + " = " + y);
+                    showTooltip(item.pageX, item.pageY, item.series.label + " di tahun " + x + " = " + y);
                 }
             } else {
                 $("#tooltip").remove();
